789d0514cc62de307eec7afa72c947914ea81595
= <thomashockaday95@gmail.com>
2018-01-13 17:51:47 +0000

    Improve mysql connection

--- src/public/index.php
+++ src/public/index.php
@@ -10,8 +10,11 @@
 
 <?php
 
-$database = $user = $password = "project";
 $host = "mysql";
+$database = "base";
+$user = "root";
+$password = "password";
+
 $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
 $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
 $tables = $query->fetchAll(PDO::FETCH_COLUMN);
